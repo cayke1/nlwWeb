c0d392120b98e365bc7a7b1d075752cf8265ca67
Cayke Daniel <caykedanielcd@gmail.com>
2022-09-19 02:59:46 -0300

    Dynamic homepage complete, connected to backend

---
+++ src/components/GameBanner.tsx
@@ -0,0 +1,17 @@
+interface GameBannerProps {
+    bannerUrl: string;
+    title: string;
+    adsCount: number;
+}
+
+export function GameBanner (props: GameBannerProps) {
+    return (
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src={props.bannerUrl} alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <span className="font-bold text-white block">{props.title}</span>
+            <span className="text-zinc-300 text-sm">{props.adsCount} anúncio(s)</span>
+          </div>
+        </a>
+    )
+}
